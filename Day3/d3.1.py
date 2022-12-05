c6b4a0b2ef343c3cddc15465a40bdb92f821cdf9
Ajsteph89 <ajsteph89@gmail.com>
2022-12-05 14:59:26 -0500

    solution found, added break to stop duplicate letters

---
+++ Day3/d3.1.py
@@ -0,0 +1,88 @@
+import sys
+from pathlib import Path
+
+
+# split each line into 2 equal parts
+# compare each half for a duplicate character
+# determine value of duplicate
+#       lower case a-z / 1-26
+#       upper case A-Z / 27-52
+# sum the values of the duplicates
+
+
+def rucksack(input):
+    pack = input.split('\n')
+    total = 0
+    characters = {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+        'i': 9,
+        'j': 10,
+        'k': 11,
+        'l': 12,
+        'm': 13,
+        'n': 14,
+        'o': 15,
+        'p': 16,
+        'q': 17,
+        'r': 18,
+        's': 19,
+        't': 20,
+        'u': 21,
+        'v': 22,
+        'w': 23,
+        'x': 24,
+        'y': 25,
+        'z': 26,
+        'A': 27,
+        'B': 28,
+        'C': 29,
+        'D': 30,
+        'E': 31,
+        'F': 32,
+        'G': 33,
+        'H': 34,
+        'I': 35,
+        'J': 36,
+        'K': 37,
+        'L': 38,
+        'M': 39,
+        'N': 40,
+        'O': 41,
+        'P': 42,
+        'Q': 43,
+        'R': 44,
+        'S': 45,
+        'T': 46,
+        'U': 47,
+        'V': 48,
+        'W': 49,
+        'X': 50,
+        'Y': 51,
+        'Z': 52,
+    }
+    for item in pack:
+        half = len(item)//2
+        first = item[:half]
+        second =  item[half:]
+        for x in first:
+            if x in second:
+                total += characters[x]
+                break
+
+    print(total)
+
+
+
+if __name__ == "__main__":
+    file = Path(sys.argv[1])
+    if Path.is_file(file):
+        rucksack(Path.read_text(file))
+    else:
+        raise TypeError("This is not a file")
